8c4e0b3c4a96997e5bb5d337c6b4110f49067688
Abhinav Singh <abnvxsin@gmail.com>
2025-05-31 08:11:22 +0530

    Fix backend crashing at port 8080

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025-present Abhinav Singh
+// Copyright (C) 2025-present Abhinav SinghAdd commentMore actions
 // This program is free software under the GNU AGPLv3.
 // See <https://www.gnu.org/licenses/> for details.
 
@@ -8,19 +8,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 
-	"github.com/a-h/templ"
-	"github.com/ab1nv/lantern/client/assets"
-	"github.com/ab1nv/lantern/client/ui/pages"
 	"github.com/ab1nv/lantern/internal/server"
 )
 
 func main() {
 	dir := flag.String("dir", "problemset", "Directory to watch or create")
-	addr := flag.String("addr", ":8080", "HTTP server address")
 	flag.Parse()
 
 	absPath, err := filepath.Abs(*dir)
@@ -32,22 +27,7 @@ func main() {
 		log.Fatalf("Error creating directory: %v", err)
 	}
 
-	fmt.Printf("📂 Using problem directory: %s\n", absPath)
+	fmt.Printf("Using directory: %s\n", absPath)
 
-	// Start backend processing
-	go server.Start(absPath)
-
-	// Set up HTTP routes
-	mux := http.NewServeMux()
-
-	// Serve embedded CSS and other assets
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.FS(assets.Assets))))
-
-	// Serve root landing page via templ
-	mux.Handle("/", templ.Handler(pages.Landing()))
-
-	fmt.Printf("🌐 Server running at http://localhost%s\n", *addr)
-	if err := http.ListenAndServe(*addr, mux); err != nil {
-		log.Fatalf("HTTP server failed: %v", err)
-	}
+	server.Start(absPath)
 }
